1e49c0b4959f406b4427d6bcd80b27064cbd0d00
Brandon Chuck <brandon.chuck@acuityconsulting.net>
2021-01-07 17:32:16 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Brandon Chuck!");
+        }
+
+    }
+}
